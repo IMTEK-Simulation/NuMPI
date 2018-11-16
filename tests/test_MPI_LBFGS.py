1a72146ca0ee72211e88ab120fce2275518c9247
AntoineSIMTEK <antoine.sanner@imtek.uni-freiburg.de>
2018-11-16 08:24:32 +0100

    TST: plot the dependence of the computation time on the number of DOF

--- tests/test_MPI_LBFGS.py
+++ tests/test_MPI_LBFGS.py
@@ -6,11 +6,17 @@ from Tools.ParallelNumpy import  ParallelNumpy
 from runtests.mpi import MPITest
 from tests.MPI_minimization_problems import MPI_Objective_Interface
 import scipy.optimize
-
+import  time
 import tests.minimization_problems as mp
 
 from PyLBGFS.MPI_LBFGS_Matrix_H import steepest_descent_wolfe2, LBFGS
 
+
+def timer(fun, *args, **kwargs):
+    start = time.perf_counter()
+    res = fun(*args, **kwargs)
+    delay = time.perf_counter() - start
+    return res, delay
 
 def test_linesearch():
     pass
@@ -92,5 +98,63 @@ def test_analytical_min(comm):
 
     np.testing.assert_allclose(res.x,PObjective.xmin(),atol=1e-16,rtol = 1e-5)
 
-
     assert np.abs(res.fun-Objective.minVal(n))< 1e-7
+
+@MPITest([1, 2,3,4,8,10,11])
+def test_ftol(comm):
+    pass
+@MPITest([1, 2,3,4,8,10,11])
+def test_gtol(comm):
+    pass
+@MPITest([1, 2,3,4,8,10,11])
+def test_g2tol(comm):
+    pass
+@MPITest([1, 2,3,4,8,10,11])
+def test_alltol(comm):
+    pass
+
+
+@pytest.mark.skip(reason="just plotting")
+def test_time_complexity(comm):
+
+
+    maxcor = 5
+    Objective = mp.Extended_Rosenbrock
+    n= np.array([10,100,1000,1e4,1e5,1e6],dtype = int)
+    t = np.zeros(len(n),dtype=float)
+    res = [None] * len(n)
+    pnp = ParallelNumpy(comm)
+    for i in range(len(n)):
+        PObjective = MPI_Objective_Interface(Objective, domain_resolution=n[i], comm=comm)
+        x0 = PObjective.startpoint()
+
+        res[i],t[i] = timer(LBFGS,PObjective.f, x0, jac=PObjective.grad, maxcor=maxcor, maxiter=100000, gtol=(1e-5), pnp=pnp)
+
+        assert res[i].success
+
+
+    if False:
+        import matplotlib.pyplot as plt
+
+        fig,ax = plt.subplots()
+        ax.plot(n,t/n,'+-', label = "time / DOF")
+        ax.plot(n, [t[i] / n[i] / res[i].nit for i in range(len(n))], '+-',label = "time per DOF per iteration")
+        ax2 = plt.twinx(ax)
+        ax2.plot(n,[res[i].nit for i in range(len(n))],'o',label = "nit")
+        ax.set_xscale('log')
+        ax.set_yscale('log')
+        ax.set_ylabel("t/n (s)")
+        ax.set_xlabel("DOF")
+        ax.legend()
+        ax2.legend()
+        #ax.plot(n,n,c='gray')
+        plt.show(block = True)
+
+
+
+
+
+
+
+
+
