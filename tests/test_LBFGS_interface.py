30e31aa279dd8c458bf51d855c15f378686e6974
AntoineSIMTEK <antoine.sanner@imtek.uni-freiburg.de>
2018-11-16 12:51:58 +0100

    TST: test for a call with row Vectors

--- tests/test_LBFGS_interface.py
+++ tests/test_LBFGS_interface.py
@@ -9,9 +9,14 @@ import tests.minimization_problems as mp
 @pytest.mark.parametrize("n",[10])
 def test_minimize_call(Objective,n):
 
+    ## Test column Vector call
     result = scipy.optimize.minimize(Objective.f,Objective.startpoint(n),jac=Objective.grad,options ={"gtol":1e-6})
     assert result.success
+    np.testing.assert_allclose(np.reshape(result.x, (-1,)), np.reshape(Objective.xmin(n), (-1,)), rtol=1e-7)
 
+    ## Test row Vectors call (like scipy and PyCo)
+    result = scipy.optimize.minimize(Objective.f, Objective.startpoint(n).reshape(-1), jac=lambda x: Objective.grad(x).reshape(-1), options={"gtol": 1e-6})
+    assert result.success, ""
     np.testing.assert_allclose(np.reshape(result.x, (-1,)), np.reshape(Objective.xmin(n), (-1,)), rtol=1e-7)
 
 def test_multiple_tol():
