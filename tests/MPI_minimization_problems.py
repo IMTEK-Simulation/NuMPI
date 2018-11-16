db39af684b1ed4b2b124ceb3fb3a1b160807630b
AntoineSIMTEK <antoine.sanner@imtek.uni-freiburg.de>
2018-11-16 08:26:08 +0100

    TST: added parallely computed quadratic Potential to the Problems to minimize

--- tests/MPI_minimization_problems.py
+++ tests/MPI_minimization_problems.py
@@ -90,6 +90,84 @@ class MPI_Extended_Rosenbrock(): #TODO: This doesn't work
 
         return np.ones((self.subdomain_resolution, 1), dtype=float)
 
+class MPI_Quadratic():
+    """
+
+    This is the Definition like in Moré et al. and not like in scipy
+
+    n should be even
+
+    :param x: 1d array
+    :return:
+    """
+    bounds = (-4, 4)
+
+    def __init__(self,domain_resolution,pnp = ParallelNumpy(),factors = None, startpoint = None):
+        comm = pnp.comm
+        nprocs = comm.Get_size()
+        rank = comm.Get_rank()
+
+        step = domain_resolution // nprocs
+
+        if rank == nprocs - 1:
+            self.subdomain_slice = slice(rank * step, None)
+            self.subdomain_location = rank * step
+            self.subdomain_resolution = domain_resolution - rank * step
+        else:
+            self.subdomain_slice  = slice(rank * step, (rank + 1) * step)
+            self.subdomain_location = rank * step
+            self.subdomain_resolution = step
+
+        #helps to select the data that has odd or even index in the global array
+        self.pnp = pnp
+
+        if factors is not None:
+            self.factors = factors[self.subdomain_slice]
+        else:
+            self.factors = np.random.random(self.subdomain_resolution)+0.1
+
+        if startpoint is not None:
+            self._startpoint = startpoint[self.subdomain_slice]
+        else :
+            self._startpoint = np.random.normal(size= self.subdomain_resolution)
+
+    def f_grad(self,x):
+        return self.pnp.sum(np.dot((x ** 2).flat, self.factors**2), axis=0),\
+               2 * self.factors.reshape(x.shape) * x
+
+    def f(self, x):
+        return self.f_grad(x)[0]
+
+    def grad(self, x):
+        return self.f_grad(x)[1]
+
+    def startpoint(self):
+        """
+        standard starting point
+        :param n:
+        :return: array of shape (1,n)
+        """
+        return self._startpoint
+
+    @staticmethod
+    def minVal(*args):
+        return 0
+
+    def xmin(self):
+        """
+        Location of minimum according to
+
+        Mori, J. J., Garbow, B. S. & Hillstrom, K. E. Testing Unconstrained Optimization Software. 25 (1981).
+
+        This function not necessarily have only one Minimum in higher dimensional Space: see e.g. 10.1162/evco.2006.14.1.119
+
+        :param n: number of DOF
+        :return: array of size n
+        """
+
+        return np.ones((self.subdomain_resolution, 0), dtype=float)
+
+
 class MPI_Objective_Interface():
     """
     creates an interface for an objective function computed serially to appear like parallel.
