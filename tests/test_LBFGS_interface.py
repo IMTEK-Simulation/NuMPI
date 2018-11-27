c8f7cc8a941c1fc7b5bc0e7f6f92369d35f62bad
AntoineSIMTEK <antoine.sanner@imtek.uni-freiburg.de>
2018-11-27 10:58:34 +0100

    TST: test that returned x and returned grad have the same shape as the starting point in the call of LBFGS

--- tests/test_LBFGS_interface.py
+++ tests/test_LBFGS_interface.py
@@ -2,6 +2,7 @@
 import numpy as np
 import scipy.optimize
 import pytest
+from PyLBFGS.MPI_LBFGS_Matrix_H import LBFGS
 
 import tests.minimization_problems as mp
 
@@ -10,16 +11,33 @@ import tests.minimization_problems as mp
 def test_minimize_call(Objective,n):
 
     ## Test column Vector call
-    result = scipy.optimize.minimize(Objective.f,Objective.startpoint(n),jac=Objective.grad,options ={"gtol":1e-6})
+    result = scipy.optimize.minimize(Objective.f,Objective.startpoint(n),jac=Objective.grad,method=LBFGS,options ={"gtol":1e-6})
     assert result.success
     np.testing.assert_allclose(np.reshape(result.x, (-1,)), np.reshape(Objective.xmin(n), (-1,)), rtol=1e-7)
 
     ## Test row Vectors call (like scipy and PyCo)
-    result = scipy.optimize.minimize(Objective.f, Objective.startpoint(n).reshape(-1), jac=lambda x: Objective.grad(x).reshape(-1), options={"gtol": 1e-6})
+    result = scipy.optimize.minimize(Objective.f, Objective.startpoint(n).reshape(-1),method=LBFGS, jac=lambda x: Objective.grad(x).reshape(-1), options={"gtol": 1e-6})
     assert result.success, ""
     np.testing.assert_allclose(np.reshape(result.x, (-1,)), np.reshape(Objective.xmin(n), (-1,)), rtol=1e-7)
 
 # TODO: test when jac is Bool
+
+@pytest.mark.parametrize("shape",[(10,),(10,1),(1,10),(2,4)])
+def test_shape_unchanged(shape):
+
+    size = np.prod(shape)
+    Objective = mp.Extended_Rosenbrock
+
+    x0 = Objective.startpoint(size).reshape(shape)
+
+
+    result=LBFGS(Objective.f, x0, jac=Objective.grad, gtol=1e-8)
+
+    assert result.success, ""
+    np.testing.assert_allclose(np.reshape(result.x, (-1,)), np.reshape(Objective.xmin(size), (-1,)), rtol=1e-5)
+
+    assert result.x.shape ==shape, "shape of result mismatch shape of startpoint"
+    assert result.jac.shape == shape, "shape of result jac mismatch shape of startpoint"
 
 def test_multiple_tol():
     pass
