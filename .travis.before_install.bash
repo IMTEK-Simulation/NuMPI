689652d52e9781a3ff99ee6a2b327e75f842d3bb
Lars Pastewka <lars.pastewka@imtek.uni-freiburg.de>
2019-04-18 14:42:57 +0200

    CI: Switched to miniconda

--- .travis.before_install.bash
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-sudo apt-get install python-numpy python-scipy
-if [ "$WITH_MPI" == "yes" ]; then
-    sudo apt-get install openmpi-bin libopenmpi-dev
-    sudo python -m pip install mpi4py
-fi
