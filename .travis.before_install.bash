92bbaa20e2593e19cb8e188d11b3bae1ae01d386
Lars Pastewka <lars.pastewka@imtek.uni-freiburg.de>
2019-04-18 14:33:43 +0200

    CI: Install Python from distribution

--- .travis.before_install.bash
+++ .travis.before_install.bash
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+sudo apt-get install python3-numpy python3-scipy
 if [ "$WITH_MPI" == "yes" ]; then
     sudo apt-get install openmpi-bin libopenmpi-dev
     sudo python -m pip install mpi4py
