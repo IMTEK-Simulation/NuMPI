7a0b2225d83e24b4d3a3b734f1c7218c3ea378d7
AntoineSIMTEK <antoine.sanner@imtek.uni-freiburg.de>
2020-06-23 09:34:52 +0200

    CI: increase numerical tolerance

--- tests/test_LBFGS_minimization_problems.py
+++ tests/test_LBFGS_minimization_problems.py
@@ -42,9 +42,9 @@ def test_analytical_min(Objective,n):
     x0 = Objective.startpoint(n)
 
     res = LBFGS(Objective.f, x0, jac=Objective.grad, maxcor=5, gtol=1e-6, maxiter=1000)
-
+    print(np.reshape(res.x, (-1,)))
     #np.testing.assert_almost_equal( res.x,Objective.xmin(n))
-    np.testing.assert_allclose(np.reshape(res.x, (-1,)), np.reshape(Objective.xmin(n),(-1,)), rtol=1e-7)
+    np.testing.assert_allclose(np.reshape(res.x, (-1,)), np.reshape(Objective.xmin(n),(-1,)), rtol=1e-6)
 
 @pytest.mark.parametrize("Objective",[mp.Trigonometric])
 @pytest.mark.parametrize("n",[10,30])
