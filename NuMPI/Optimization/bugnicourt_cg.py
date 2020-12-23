985f9e314b44d7bd8bec90b2a633a9e2bb8977f0
AntoineSIMTEK <antoine.sanner@imtek.uni-freiburg.de>
2020-12-23 15:12:09 +0100

    ENH: apply the constraint on the initial guess the same way as in the bulk of the code

--- NuMPI/Optimization/bugnicourt_cg.py
+++ NuMPI/Optimization/bugnicourt_cg.py
@@ -32,14 +32,23 @@ def constrained_conjugate_gradients(fun, hessp,
 
     mask_neg = x <= 0
     x[mask_neg] = 0.0
-    mask_res = residual >= 0
 
-    mask_na = np.logical_and(mask_neg, mask_res)
-    residual[mask_na] = 0.0
     if mean_val is not None:
+        #
+        mask_nonzero = x > 0
+        N_mask_nonzero = comm.sum(np.count_nonzero(mask_nonzero))
+        residual = residual \
+            - comm.sum(residual[mask_nonzero]) / N_mask_nonzero
+
+    '''Apply the admissible Lagrange multipliers.'''
+    mask_res = residual >= 0
+    mask_bounded = np.logical_and(mask_neg, mask_res)
+    residual[mask_bounded] = 0.0
+
+    if mean_val is not None:
+        #
         mask_nonzero = residual != 0
         N_nonzero = comm.sum(np.count_nonzero(mask_nonzero))
-
         residual[mask_nonzero] = residual[mask_nonzero] - comm.sum(
             residual[mask_nonzero]) / N_nonzero
     '''INITIAL DESCENT DIRECTION'''
@@ -55,8 +64,7 @@ def constrained_conjugate_gradients(fun, hessp,
             hessp_val = hessp(des_dir)
         else:
             raise ValueError('hessp function has to take max 1 arg (descent '
-                             'dir)\
-                                    or 2 args (x, descent direction)')
+                             'dir) or 2 args (x, descent direction)')
         denominator_temp = comm.sum(des_dir.T * hessp_val)
         # TODO: here we could spare one FFT
 
@@ -118,9 +126,12 @@ def constrained_conjugate_gradients(fun, hessp,
 
         des_dir_old = des_dir
         des_dir = -residual + beta * des_dir_old
+
+        # TODO: this is useless
         des_dir[np.logical_not(mask_bounded)] = -residual[
             np.logical_not(mask_bounded)] + beta * des_dir_old[
             np.logical_not(mask_bounded)]
+
         des_dir[mask_bounded] = 0
 
         if callback:
