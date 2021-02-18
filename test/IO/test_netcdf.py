c69314432d63c67e38bc2a69c262869ad3b3be30
AntoineSIMTEK <antoine.sanner@imtek.uni-freiburg.de>
2021-02-18 14:19:59 +0100

    MAINT: update license HEADERS

--- test/IO/test_netcdf.py
+++ test/IO/test_netcdf.py
@@ -1,3 +1,26 @@
+#
+# Copyright 2021 Antoine Sanner
+#
+# ### MIT license
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
 """
 
 Licence HEADER from muspectre
@@ -70,17 +93,18 @@ class DomainDecomposition():
             self.nb_subdomain_grid_pts[0] = step
 
 
-# @pytest.mark.parametrize("nb_grid_pts", [(11, 23), (11, 23, 7)])
 @pytest.fixture(params=[(11,), (11, 23), (11, 23, 7)])
 def self(request, comm):
+    # This fixture mimics the setup in the unit test class.
+    # This test was written in the unittest syntax before.
+
     class self:
         pass
 
     self.nb_grid_pts = request.param
     self.tensor_shape = tuple(list(self.nb_grid_pts) + [3, 3])
     self.scalar_grid = np.arange(np.prod(self.nb_grid_pts)).reshape(self.nb_grid_pts)
-    self.tensor_grid = np.arange(np.prod(self.tensor_shape)) \
-        .reshape(self.tensor_shape)
+    self.tensor_grid = np.arange(np.prod(self.tensor_shape)).reshape(self.tensor_shape)
 
     self.communicator = comm
 
