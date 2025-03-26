04dce30c92946032d73fb82962121846b21ffef3
Lars Pastewka <lars.pastewka@imtek.uni-freiburg.de>
2025-03-26 20:09:34 +0100

    MAINT: Cleaned up print and debug outout

--- NuMPI/Optimization/LBFGS.py
+++ NuMPI/Optimization/LBFGS.py
@@ -23,9 +23,7 @@
 # SOFTWARE.
 #
 
-# skip undefined names
-# flake8: noqa F821
-
+import logging
 
 import numpy as np
 
@@ -33,6 +31,8 @@ from .. import MPI
 from ..Tools import Reduction
 from .LineSearch import scalar_search_wolfe2
 from .Result import OptimizeResult
+
+_log = logging.getLogger(__name__)
 
 
 def donothing(*args, **kwargs):
@@ -64,11 +64,14 @@ def steepest_descent_wolfe2(x0, f_gradf, pnp=None, maxiter=10, args=(), **kwargs
     # if (iter.eq. 0.and..not.boxed) then
     #    stp = min(one / dnorm, stpmx)
 
-    alpha, phi, phi0, derphi = scalar_search_wolfe2(_phi_phiprime,
-                                                    maxiter=maxiter,
-                                                    phi0=phi0, derphi0=derphi0,
-                                                    step=min(1., 1. / np.sqrt(
-                                                        -derphi0)), **kwargs)
+    alpha, phi, phi0, derphi = scalar_search_wolfe2(
+        _phi_phiprime,
+        maxiter=maxiter,
+        phi0=phi0,
+        derphi0=derphi0,
+        step=min(1.0, 1.0 / np.sqrt(-derphi0)),
+        **kwargs,
+    )
 
     assert derphi is not None, "Line Search in first steepest descent failed"
     x = x0 - grad0 * alpha
@@ -76,10 +79,25 @@ def steepest_descent_wolfe2(x0, f_gradf, pnp=None, maxiter=10, args=(), **kwargs
     return x, gradf, x0, grad0, phi, phi0, derphi
 
 
-def l_bfgs(fun, x, args=(), jac=None, x_old=None, maxcor=10, gtol=1e-5, ftol=2.2e-9, maxiter=15000, maxls=20,
-           linesearch_options=dict(c1=1e-3, c2=0.9), disp=None, pnp=Reduction(MPI.COMM_WORLD), store_iterates=None,
-           printdb=donothing, callback=None, **options):
-    """
+def l_bfgs(
+    fun,
+    x,
+    args=(),
+    jac=None,
+    x_old=None,
+    maxcor=10,
+    gtol=1e-5,
+    ftol=2.2e-9,
+    maxiter=15000,
+    maxls=20,
+    linesearch_options=dict(c1=1e-3, c2=0.9),
+    disp=None,
+    comm=MPI.COMM_WORLD,
+    store_iterates=None,
+    callback=None,
+    **options,
+):
+    r"""
     Limited-memory L-BFGS optimization with MPI parallelization.
 
     The optimization converges if |grad|_{\infty} <= gtol or <= ftol is satisfied.
@@ -93,13 +111,18 @@ def l_bfgs(fun, x, args=(), jac=None, x_old=None, maxcor=10, gtol=1e-5, ftol=2.2
     args : tuple, optional
         Extra arguments passed to the objective function and its derivatives (Jacobian).
     jac : bool or callable, optional
-        Method for computing the gradient vector. If it is a callable, it should be a function that returns the gradient vector. If it is a Boolean and is True, then `fun` should return the gradient along with the objective function.
+        Method for computing the gradient vector. If it is a callable, it should be a
+        function that returns the gradient vector. If it is a Boolean and is True, then
+        `fun` should return the gradient along with the objective function.
     x_old : ndarray, optional
-        Initial guess for the previous value of x. If None, a steepest descent step will be performed.
+        Initial guess for the previous value of x. If None, a steepest descent step will
+        be performed.
     maxcor : int, optional
-        The maximum number of variable metric corrections used to define the limited memory matrix.
+        The maximum number of variable metric corrections used to define the limited
+        memory matrix.
     gtol : float, optional
-        The iteration will stop when max{|proj g_i | i = 1, ..., n} <= gtol where pg_i is the i-th component of the projected gradient.
+        The iteration will stop when max{|proj g_i | i = 1, ..., n} <= gtol where pg_i
+        is the i-th component of the projected gradient.
     ftol : float, optional
         The iteration will stop when (f^k - f^{k+1})/max{|f^k|,|f^{k+1}|,1} <= ftol.
     maxiter : int, optional
@@ -110,12 +133,10 @@ def l_bfgs(fun, x, args=(), jac=None, x_old=None, maxcor=10, gtol=1e-5, ftol=2.2
         A dictionary with optional settings for the line search parameters.
     disp : int, optional
         If disp is a positive integer, convergence messages will be printed.
-    pnp : Reduction, optional
-        Parallelization object from the NuMPI package.
+    comm : MPI.Comm, optional
+        MPI communicator.
     store_iterates : bool, optional
         If True, the result at each iteration is stored in a list.
-    printdb : callable, optional
-        Debugging print function.
     callback : callable, optional
         Called after each iteration.
     options : dict, optional
@@ -124,17 +145,22 @@ def l_bfgs(fun, x, args=(), jac=None, x_old=None, maxcor=10, gtol=1e-5, ftol=2.2
     Returns
     -------
     result : OptimizeResult
-        The optimization result represented as a OptimizeResult object. Important attributes are: x the solution array, success a Boolean flag indicating if the optimizer exited successfully and message which describes the cause of the termination.
+        The optimization result represented as a OptimizeResult object. Important
+        attributes are: x the solution array, success a Boolean flag indicating if the
+        optimizer exited successfully and message which describes the cause of the
+        termination.
     """
     # user can provide x in the shape of his convenience
     # but we will work with the shape (-1, 1)
     original_shape = x.shape
 
+    pnp = Reduction(comm)
+
     if callback is None:
         callback = donothing
 
-    # print("jac = {}, type = {}".format(jac, type(jac)))
     if jac is True:
+
         def fun_grad(x, *args):
             """
             This function evaluates the objective function and its gradient at the point x.
@@ -156,8 +182,7 @@ def l_bfgs(fun, x, args=(), jac=None, x_old=None, maxcor=10, gtol=1e-5, ftol=2.2
             return f, grad.reshape((-1, 1))
 
     elif jac is False:
-        raise NotImplementedError(
-            "Numerical evaluation of gradient not implemented")
+        raise NotImplementedError("Numerical evaluation of gradient not implemented")
     else:
         # function and gradient provided sepatately
 
@@ -188,9 +213,9 @@ def l_bfgs(fun, x, args=(), jac=None, x_old=None, maxcor=10, gtol=1e-5, ftol=2.2
 
     if x_old is None:
         x_old = x.copy()
-        x, grad, x_old, grad_old, phi, phi_old, derphi = \
-            steepest_descent_wolfe2(x_old, fun_grad, pnp=pnp, maxiter=maxls,
-                                    args=args, **linesearch_options)
+        x, grad, x_old, grad_old, phi, phi_old, derphi = steepest_descent_wolfe2(
+            x_old, fun_grad, pnp=pnp, maxiter=maxls, args=args, **linesearch_options
+        )
     else:
         # phi_old is never used, except for the convergence
         phi_old, grad_old = fun_grad(x_old, *args)
@@ -208,38 +233,45 @@ def l_bfgs(fun, x, args=(), jac=None, x_old=None, maxcor=10, gtol=1e-5, ftol=2.2
     Ylist = []  # history of gradient differences
     R = np.zeros((0, 0))
 
-    grad2 = pnp.sum(grad ** 2)
+    grad2 = pnp.sum(grad**2)
 
     alpha = 0  # line search step size
 
     if disp:
-        if MPI.COMM_WORLD.rank == 0:
+        if comm.rank == 0:
             iteration_title = "iteration"
             phi_title = "f"
             phi_change_title = "Δf"
             max_grad_title = "max ∇ f"
-            norm_grad_title = "||∇ f||²"
+            norm_grad_title = "||∇ f||"
             print(
-                f"{iteration_title:<10} {phi_title:<10} {phi_change_title:<10} {max_grad_title:<10} {norm_grad_title:<10}")
-            print(f"---------- ---------- ---------- ---------- ----------")
+                f"{iteration_title:<10} {phi_title:<10} {phi_change_title:<10} "
+                f"{max_grad_title:<10} {norm_grad_title:<10}"
+            )
+            print("---------- ---------- ---------- ---------- ----------")
+
+    STgrad_prev = None
+    YTgrad_prev = None
+    p1 = None
+    p2 = None
+    D = None
+    YTY = None
 
     # Start loop
-    # printdb(k)
     while True:
         # Update Sk,Yk
-        # print("k= {}".format(k))
         if iteration > maxcor:
             # S = np.roll(S, -1)
             # S[:, -1] = (x - x_old).flat
 
             Slist[:-1] = Slist[1:]
-            Slist[-1] = (x - x_old)
+            Slist[-1] = x - x_old
 
             # Y = np.roll(Y, -1)
             # Y[:, -1] = (grad - grad_old).flat
 
             Ylist[:-1] = Ylist[1:]
-            Ylist[-1] = (grad - grad_old)
+            Ylist[-1] = grad - grad_old
 
         else:
             # S = np.hstack([S, x - x_old])
@@ -261,69 +293,67 @@ def l_bfgs(fun, x, args=(), jac=None, x_old=None, maxcor=10, gtol=1e-5, ftol=2.2
         callback(x)
 
         max_grad = pnp.max(np.abs(grad))
-        norm_grad = grad2
+        norm_grad = np.sqrt(grad2)
         phi_change = phi_old - phi
 
         if disp:
-            if MPI.COMM_WORLD.rank == 0:
-                print(f"{iteration:<10} {phi:<10.7g} {phi_change:<10.7g} {max_grad:<10.7g} {norm_grad:<10.7g}")
+            if comm.rank == 0:
+                print(
+                    f"{iteration:<10} {phi:<10.7g} {phi_change:<10.7g} {max_grad:<10.7g} {norm_grad:<10.7g}"
+                )
 
-        if (max_grad < gtol):
-            print("CONVERGED because gradient tolerance was reached")
-            result = OptimizeResult({
-                'success': True,
-                'x': x.reshape(
-                    original_shape),
-                'fun': phi,
-                'jac': grad.reshape(
-                    original_shape),
-                'nit': iteration,
-                'message':
-                    'CONVERGENCE: '
-                    'NORM_OF_GRADIENT_<=_GTOL',
-                'iterates': iterates,
-                'max_grad': max_grad,
-                'norm_grad': norm_grad,
-                'phi_change': phi_change
-            })
+        if max_grad < gtol:
+            _log.info("CONVERGED because gradient tolerance was reached")
+            result = OptimizeResult(
+                {
+                    "success": True,
+                    "x": x.reshape(original_shape),
+                    "fun": phi,
+                    "jac": grad.reshape(original_shape),
+                    "nit": iteration,
+                    "message": "CONVERGENCE: " "NORM_OF_GRADIENT_<=_GTOL",
+                    "iterates": iterates,
+                    "max_grad": max_grad,
+                    "norm_grad": norm_grad,
+                    "phi_change": phi_change,
+                }
+            )
             return result
 
-        if (phi_change <= ftol * max((1, abs(phi), abs(phi_old)))):
-            print("CONVERGED because function tolerance was reached")
-            result = OptimizeResult({
-                'success': True,
-                'x': x.reshape(
-                    original_shape),
-                'fun': phi,
-                'jac': grad.reshape(
-                    original_shape),
-                'nit': iteration,
-                'message':
-                    'CONVERGENCE: '
-                    'REL_REDUCTION_OF_F_<=_FACTR*EPSMCH',
-                'iterates': iterates,
-                'max_grad': max_grad,
-                'norm_grad': norm_grad,
-                'phi_change': phi_change
-            })
+        if phi_change <= ftol * max((1, abs(phi), abs(phi_old))):
+            _log.info("CONVERGED because function tolerance was reached")
+            result = OptimizeResult(
+                {
+                    "success": True,
+                    "x": x.reshape(original_shape),
+                    "fun": phi,
+                    "jac": grad.reshape(original_shape),
+                    "nit": iteration,
+                    "message": "CONVERGENCE: " "REL_REDUCTION_OF_F_<=_FACTR*EPSMCH",
+                    "iterates": iterates,
+                    "max_grad": max_grad,
+                    "norm_grad": norm_grad,
+                    "phi_change": phi_change,
+                }
+            )
 
             return result
 
         if iteration > maxiter:
-            print("CONVERGED because maximum number of iterations reached")
-            result = OptimizeResult({
-                'success': False,
-                'x': x.reshape(
-                    original_shape),
-                'fun': phi,
-                'jac': grad.reshape(
-                    original_shape),
-                'nit': iteration,
-                'iterates': iterates,
-                'max_grad': max_grad,
-                'norm_grad': norm_grad,
-                'phi_change': phi_change
-            })
+            _log.info("CONVERGED because maximum number of iterations reached")
+            result = OptimizeResult(
+                {
+                    "success": False,
+                    "x": x.reshape(original_shape),
+                    "fun": phi,
+                    "jac": grad.reshape(original_shape),
+                    "nit": iteration,
+                    "iterates": iterates,
+                    "max_grad": max_grad,
+                    "norm_grad": norm_grad,
+                    "phi_change": phi_change,
+                }
+            )
 
             return result
         ###########
@@ -331,41 +361,44 @@ def l_bfgs(fun, x, args=(), jac=None, x_old=None, maxcor=10, gtol=1e-5, ftol=2.2
 
         STgrad = np.array([pnp.dot(si.T, grad) for si in Slist]).reshape(-1, 1)
         YTgrad = np.array([pnp.dot(yi.T, grad) for yi in Ylist]).reshape(-1, 1)
-        # STgrad = pnp.dot(S.T, grad)
-        # YTgrad = pnp.dot(Y.T, grad)
 
         if iteration > maxcor:
             S_now_T_grad_prev = np.roll(STgrad_prev, -1)
-            S_now_T_grad_prev[-1] = - alpha * gamma * grad2prev - alpha * (
-                    STgrad_prev.T.dot(p1) + gamma * YTgrad_prev.T.dot(p2))
+            S_now_T_grad_prev[-1] = -alpha * gamma * grad2prev - alpha * (
+                STgrad_prev.T.dot(p1) + gamma * YTgrad_prev.T.dot(p2)
+            )
         else:  # straightforward Version
             S_now_T_grad_prev = np.array(
-                [pnp.dot(si.T, grad_old) for si in Slist]).reshape(-1, 1)
+                [pnp.dot(si.T, grad_old) for si in Slist]
+            ).reshape(-1, 1)
 
         if iteration > maxcor:
-            R = np.roll(R, (-1, -1),
-                        axis=(0, 1))  # mxm Matrix hold by all Processors
+            R = np.roll(R, (-1, -1), axis=(0, 1))  # mxm Matrix hold by all Processors
             R[-1, :] = 0
             STym1 = STgrad - S_now_T_grad_prev
             R[:, -1] = STym1.flat  # O(m x n)
 
         elif iteration == 1:
-            # Rm = np.triu(pnp.dot(S.T, Y))
-            R = np.triu(np.array(
-                [[pnp.dot(si.T, yi).item() for yi in Ylist] for si in Slist]))
-            # print(R)
+            R = np.triu(
+                np.array([[pnp.dot(si.T, yi).item() for yi in Ylist] for si in Slist])
+            )
         else:
             # Rm = np.vstack([Rm, np.zeros(k - 1)])
             # Rm = np.hstack([Rm, pnp.dot(S.T, Y[:, -1]).reshape(k, 1)])
             R = np.vstack([R, np.zeros(iteration - 1)])
-            R = np.hstack([R, np.array(
-                [pnp.dot(si.T, Ylist[-1]) for si in Slist]).reshape(iteration, 1)])
+            R = np.hstack(
+                [
+                    R,
+                    np.array([pnp.dot(si.T, Ylist[-1]) for si in Slist]).reshape(
+                        iteration, 1
+                    ),
+                ]
+            )
         if iteration > maxcor:
             D = np.roll(D, (-1, -1), axis=(0, 1))
             # D[-1,-1] = np.dot(Y[:,-1],Y[:,-1])# yk-1Tyk-1 # TOOPTIMIZE
             D[-1, -1] = R[-1, -1]
         else:
-            # D = np.diag(np.einsum("ik,ik -> k", S, Y))
             D = np.diag(R.diagonal())
         assert D[-1, -1] > 0, "k = {}: ".format(iteration)  # Assumption of Theorem 2.2
 
@@ -376,27 +409,22 @@ def l_bfgs(fun, x, args=(), jac=None, x_old=None, maxcor=10, gtol=1e-5, ftol=2.2
         else:
             # YTYm = pnp.dot(Y.T, Y)
             YTY = np.array(
-                [[pnp.dot(yi1.T, yi2).item() for yi2 in Ylist] for yi1 in
-                 Ylist])
+                [[pnp.dot(yi1.T, yi2).item() for yi2 in Ylist] for yi1 in Ylist]
+            )
         # Step 5.
-        gamma = D[-1, -1] / YTY[
-            -1, -1]  # n.b. D[-1,-1] = sk-1T yk-1 = yk-1T sk-1
+        gamma = D[-1, -1] / YTY[-1, -1]  # n.b. D[-1,-1] = sk-1T yk-1 = yk-1T sk-1
 
         # Step 6. and 7. together
         Rinv = np.linalg.inv(R)
         RiSg = Rinv.dot(STgrad)
         p1 = Rinv.T.dot(D + gamma * YTY).dot(RiSg) - gamma * Rinv.T.dot(YTgrad)
-        p2 = - RiSg  # TODO
+        p2 = -RiSg  # TODO
 
-        # temphstack=np.hstack([S, gamma * Y])
-
-        # Hgradm = gamma * grad + S.dot(p1)  + gamma * Y.dot(p2)
         Hgrad = gamma * grad
         for si, yi, p1i, p2i in zip(Slist, Ylist, p1.flat, p2.flat):
             Hgrad += si * p1i.item() + gamma * yi * p2i.item()
 
         phi_old = float(phi)
-        # printdb("Linesearch: ")
         grad_old[:] = grad
         x_old[:] = x
 
@@ -412,28 +440,26 @@ def l_bfgs(fun, x, args=(), jac=None, x_old=None, maxcor=10, gtol=1e-5, ftol=2.2
             phi0=phi,
             derphi0=pnp.dot(grad.T, -Hgrad).item(),
             maxiter=maxls,
-            **linesearch_options)
-
-        printdb("derphi: {}".format(derphi))
+            **linesearch_options,
+        )
 
         if derphi is None:
-            print("line-search did not converge")
-            result = OptimizeResult({
-                'success': False,
-                'x': x.reshape(
-                    original_shape),
-                'fun': phi,
-                'jac': grad.reshape(
-                    original_shape),
-                'nit': iteration,
-                'iterates': iterates,
-                'message':
-                    'CONVERGENCE: '
-                    'line-search did not converge',
-                'max_grad': max_grad,
-                'norm_grad': norm_grad,
-                'phi_change': phi_change
-            })
+            if comm.rank == 0:
+                _log.info("line-search did not converge")
+            result = OptimizeResult(
+                {
+                    "success": False,
+                    "x": x.reshape(original_shape),
+                    "fun": phi,
+                    "jac": grad.reshape(original_shape),
+                    "nit": iteration,
+                    "iterates": iterates,
+                    "message": "CONVERGENCE: " "line-search did not converge",
+                    "max_grad": max_grad,
+                    "norm_grad": norm_grad,
+                    "phi_change": phi_change,
+                }
+            )
             return result
 
         x = x - Hgrad * alpha
@@ -441,16 +467,16 @@ def l_bfgs(fun, x, args=(), jac=None, x_old=None, maxcor=10, gtol=1e-5, ftol=2.2
         if store_iterates:
             iterate = OptimizeResult(
                 {
-                    'x': x.copy().reshape(original_shape),
-                    'fun': phi,
-                    'jac': grad.reshape(original_shape),
-                    'max_grad': max_grad,
-                    'norm_grad': norm_grad,
-                    'phi_change': phi_change
-                })
+                    "x": x.copy().reshape(original_shape),
+                    "fun": phi,
+                    "jac": grad.reshape(original_shape),
+                    "max_grad": max_grad,
+                    "norm_grad": norm_grad,
+                    "phi_change": phi_change,
+                }
+            )
             iterates.append(iterate)
 
-        printdb("k = {}".format(iteration))
         iteration = iteration + 1
 
         STgrad_prev = STgrad.copy()
